7fb66570c4ee8ef07c8405c843498cad526077fd
Christopher Demicoli <christopher.demicoli@outlook.com>
2015-01-09 19:19:28 +0100

    Some work on the Book Leave Section

---
+++ Interface/Controllers/LeaveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Interface.Controllers
+{
+    public class LeaveController : Controller
+    {
+        // GET: Leave
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [NeedsLogin]
+        public ActionResult Book()
+        {
+            return View();
+        }
+    }
+}
