598fd8ffc1d29adf916caea669db01343ca00359
Christopher Demicoli <christopher.demicoli@outlook.com>
2015-01-12 21:39:55 +0100

    This fixes #1

--- Logic/Extensions.cs
+++ Logic/Extensions.cs
@@ -1,9 +1,14 @@
-﻿using System.Net.Mail;
+﻿using System;
+using System.Net.Mail;
 
 namespace Logic
 {
     public static class Extensions
     {
+        public static string ToMomentTimeToMomentTime(this DateTime dateTime)
+        {
+            return dateTime.ToString("u");
+        }
         public static MailAddress GetMailAddress(this Employee e)
         {
             return new MailAddress(e.Email, e.Name + " " + e.Surname);
